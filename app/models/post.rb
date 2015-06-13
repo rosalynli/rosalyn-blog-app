8e1921811888d04f3ebad69a893b7e5b12b71711
Rosalyn <rosalyn0425@gmail.com>
2015-06-13 21:41:02 +0000

    implement twitter check

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,20 @@ class Post < ActiveRecord::Base
     validates :title, presence: true 
     
     def instance_report()
-        puts "This is post #{id}, my title is #{title}, and here's what I have to say: #{body}"
+        puts "This is post #{id}, my title is #{title}, and here's what I have 
+        to say: #{body}, I was born #{created_at} and updated #{updated_at}"
     end
+    
+    def self.class_report
+        puts "Hi, I'm the POST CLASS and I have #{Post.count} messages" 
+    end
+
+    def twitter_check
+        if title.length <= 140
+        puts "You are ready for twitter"
+        else
+        puts "Please shorten your message"
+        end
+    end
+
 end
