e6441a64f25c4fa6d1638f970fce95fc7015fd75
Rosalyn <rosalyn0425@gmail.com>
2015-06-20 18:45:04 +0000

    implemented based show feature, adding more content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
     
+    #READ ACTIONS
     def index
     end
     
+    def show
+        id = params["id"]
+        @post = Post.find(id)
+        render "show.html.erb"
+    end
+    
+    def select_post
+        params.require(:post).require(:id)
+    end
+
 end
